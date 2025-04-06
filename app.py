8f5bc5d039b9f98ab562ebec0bdc9559e225870e
mohithk5 <kuntamukkalamohith@gmail.com>
2025-04-06 18:55:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 import streamlit as st
-from recommendation_engine import scrape_url, prepare_input, get_recommendations,traced_get_recommendations
+from recommendation_engine import scrape_url, prepare_input, get_recommendations
 from evaluate import evaluate
 import json
 import os
@@ -22,7 +22,7 @@ url = st.text_input("Optional Job Description URL")
 if st.button("Recommend Assessments"):
     jd_text = scrape_url(url) if url else ""
     query_text = prepare_input(query, duration, jd_text)
-    recommendations = traced_get_recommendations(query_text, top_k=10, max_duration=duration)
+    recommendations = get_recommendations(query_text, top_k=10, max_duration=duration)
     st.write("Query Input:", query_text)
     st.subheader("Top Recommendations")
     st.table(recommendations)
